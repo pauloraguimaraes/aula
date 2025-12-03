c42194ac2d09b1564cfd4cecb8d15cdb2483d9b8
pauloraguimaraes <76212164+pauloraguimaraes@users.noreply.github.com>
2025-12-02 21:40:53 -0300

    Create arquivo1.jsx
    criação do arquivo teste

---
+++ arquivo1.jsx
@@ -0,0 +1,17 @@
+import { useParams } from 'react-router-dom'; // 1. Importar o hook
+
+function Produto() {
+  // 2. Extrair os parâmetros da URL. O nome deve ser igual ao definido na rota (:id)
+  const { idProduto } = useParams(); 
+  
+  // 3. Onde você usaria o useEffect para buscar o produto[idProduto] na API
+  // useEffect(() => { fetchProduto(idProduto) }, [idProduto]);
+
+  return (
+    <div>
+      <h2>Página do Produto</h2>
+      <p>Você está visualizando o produto de ID: <strong>{idProduto}</strong></p>
+    </div>
+  );
+}
+// export default Produto;
